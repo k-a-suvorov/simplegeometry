88dc44914a5c747532f19551f5ddc1cb47225ce9
Konstantin Suvorov <47966800+k-a-suvorov@users.noreply.github.com>
2020-02-11 23:13:58 +0300

    Add files via upload
    Refactoring code: remove some functions and add functions of an areas of different figures: 
    Triangle
    Rectangle
    Ellipse
    Trapeze
    Parallelogram
    Cylinder
    Sphere

--- geometry.py
+++ geometry.py
@@ -1,22 +1,149 @@
+#inport the module of the user math functions
 import mathdefs as md
 try:
+	#Initialize block
+	print('Calculate the areas of different figures')
 	
-		#Initialize block
-	a = float(input('Insert width of Rectangle: '))
-	b = float(input('Insert height of Rectangle: '))
+	switch = True
+	choice = int(input('Press 1 for triangle, \r\n2 for rectangle, \r\n3 for ellipse, \r\n4 for Parallelogram, \r\n5 for trapeze, \r\n6 for sphere, \r\n7 for cylinder \r\n>>> '))
+	
+	if ((choice < 1) and (choice > 7)):
+		print('The proram was aborted!')
+		switch = False
 		
-	result_perimeter = md.perimeter_rect(a,b)
-	result_area = md.area_rect(a,b)
-	result_diagonal = md.diagonal_rect(a,b)
+	elif (choice == 1):
+		
+		#Initialize block
+		a = input('Insert footing of Triangle: ')
+		h = input('Insert height of Triangle: ')
+		
+		#Processing block
+		a = float(a)
+		h = float(h)		
+		if ((a == 0) or (h == 0) or (a < 0) or (h < 0)):
+			print('The proram was aborted!')
+			switch = False
+			
+		result_triangle = md.area_triangle(a, h)
 		
 		#Print block
-	print(f'The perimeter of the rectangle: {result_perimeter}')
-	print(f'The area of the rectangle: {result_area}')
-	print(f'The diagonal of the rectangle: {result_diagonal}')
-
+		print(f'The area of the triangle: {result_triangle}')
+	
+	elif (choice == 2):
+		
+		#Initialize block
+		a = input('Insert width of Rectangle: ')
+		b = input('Insert height of Rectangle: ')
+		
+		#Processing block
+		a = float(a)
+		b = float(b)
+		if ((a == 0) or (b == 0) or (a < 0) or (b < 0)):
+			print('The proram was aborted!')
+			switch = False
+			
+		result_rectangle = md.area_rectangle(a, b)
+		
+		#Print block
+		print(f'The area of the rectangle: {result_rectangle}')
+	
+	elif (choice == 3):
+		
+		#Initialize block
+		r1 = input('Insert 1th radius of Ellipse: ')
+		r2 = input('Insert 2th radius of Ellipse: ')
+		
+		#Processing block
+		r1 = float(r1)
+		r2 = float(r2)
+		if ((r1 == 0) or (r2 == 0) or (r1 < 0) or (r2 < 0)):
+			print('The proram was aborted!')
+			switch = False
+			
+		result_ellipse = md.area_ellipse(r1, r2)
+		
+		#Print block
+		print(f'The area of the rectangle: {result_ellipse}')
+	
+	elif (choice == 4):
+		
+		#Initialize block
+		a = input('Insert width of Parallelogram: ')
+		h = input('Insert height of Parallelogram: ')
+		
+		#Processing block
+		a = float(a)
+		h = float(h)
+		
+		if ((a == 0) or (h == 0) or (a < 0) or (h < 0)):
+			print('The proram was aborted!')
+			switch = False
+			
+		result_parallelogram = md.area_parallelogram(a, h)
+		print(f'The area of the rectangle: {result_parallelogram}')
+	
+	elif (choice == 5):
+		
+		#Initialize block
+		a = input('Insert width of Trapeze: ')
+		b = input('Insert width of Trapeze: ')
+		h = input('Insert height of Trapeze: ')
+		
+		#Processing block
+		a = float(a)
+		b = float(b)
+		h = float(h)
+		if ((a == 0) or (b == 0) or (h == 0)):
+			
+			print('The proram was aborted!')
+			switch = False
+			
+		if ((a < 0) or (h < 0) or (b < 0)):
+			print('The proram was aborted!')
+			switch = False	
+			
+		result_trapeze = md.area_trapeze(a, b, h)
+		
+		#Print block
+		print(f'The area of the rtrapeze: {result_trapeze}')
+	
+	elif (choice == 6):
+		
+		#Initialize block
+		r = input('Insert radius of cylinder: ')
+		h = input('Insert height of cylinder: ')
+		
+		#Processing block
+		r = float(r)
+		h = float(h)
+		if ((r == 0) or (h == 0) or (r < 0) or (h < 0)):
+			print('The proram was aborted!')
+			switch = False
+			
+		result_cylinder = md.area_cylinder(r, h)
+		
+		#Print block
+		print(f'The area of the cylinder: {result_cylinder}')
+	
+	elif (choice == 7):	
+		
+		#Initialize block
+		r = input('Insert radius of sphere: ')
+		
+		#Processing block
+		r = float(r)
+		if ((r == 0) or (r < 0)):
+			print('The proram was aborted!')
+			switch = False		
+			
+		result_sphere = md.area_sphere(r)
+		
+		#Print block
+		print(f'The area of the sphere: {result_sphere}')
+		
 except ValueError:
-    print("You have some mistake of userinput current float Value")
+    print("You have some mistake of userinput Value")
 except TypeError:
-   print("You have some mistake of userinput float Value!")
+   print("You have some mistake of type Value!")
 except SystemError:
 	print("This is system mistake! Please don't panic! Relax!" )
--- mathdefs.py
+++ mathdefs.py
@@ -2,15 +2,40 @@ import math
 import random
 
 #Processing block
-def perimeter_rect(a, b):
-	p = 2 *(a + b)
-	return p
-	
-def area_rect(a, b):
+
+#math fanctions of areas of the geometrical figure
+
+#An Area of the triangle
+def area_triangle(a, h):
+	s = (1 / 2) * (a * h)
+	return s
+
+#An Area of the rectangle
+def area_rectangle(a, b):
 	s = a * b
 	return s
-		
-def diagonal_rect(a, b):
-	d = round(math.sqrt((a * a) + (b * b)), 3)
-	return d	
-		
+
+#An Area of the ellipse		
+def area_ellipse(r1, r2):
+	s = round(math.pi * (r1 * r2), 2)
+	return s
+
+#An Area of the parallelogram	
+def area_parallelogram(a, h):
+	s = a * h
+	return s
+
+#An Area of the trapeze
+def area_trapeze(a, b, h):
+	s = round(((a + b) / (2 * h)), 2)
+	return s
+
+#An Area of the cylinder
+def area_cylinder(r, h):
+	s = round((2 * math.pi) * (r + h), 2)
+	return s
+
+#An Area of the sphere
+def area_sphere(r):
+	s = round((4 * math.pi) * (r * r), 2)
+	return s
